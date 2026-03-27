818f4b97fcb5b338758e38180a3f10e703e6f842
Matt Jennings <matt@mattjennings.net>
2026-03-27 15:46:37 -0700

    added code

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 /*
-Implement a tabs component
+Accordion
+
+Build an accordion component that a displays a list of vertically stacked sections with each containing a title and content snippet
 
 Steps:
 1. Clarify requirements (5 min)
@@ -10,66 +12,37 @@ Steps:
    3. What do I need to know for the input or output
    4. How do I manage the state and recommend "controlled" state
       1. PERSONAL THOUGHT: Will I need local storage
-      2. Will this be shareable
-      3. Maybe use React context
 
 Plan:
 1. Input will be whatever I want it to be (and include the key)
 
-const input = [
-  {
-    tabText: 'Tab 1',
-    tabContent: 'Content 1',
-    tabId: 'tab-1'
-  },
-  {
-    tabText: 'Tab 2',
-    tabContent: 'Content 2',
-    tabId: 'tab-2'
-  },
-  {
-    tabText: 'Tab 3',
-    tabContent: 'Content 3',
-    tabId: 'tab-3'
-  }  
-];
+2. Will include an `input` variable that has the accordion content
 
-2. Components:
-   1. Tabs
-      List of 3 buttons
-      A button is more semantically correct for something that is clickable (not navigation, which is for links)
-   2. TabBtn
-      Individual button with with an `id` of `tab-1`, `tab-2`, etc. and tabText
-   3. TabContent
-      Displayed outside of the `Tab` and include `data-id` of `tab-1`, etc. and include TabContent
+3. Components:
+   1. Accordions
+   2. AccordionBtn
+   3. AccordionContent
 */
-import {useState, useEffect, createContext, useContext} from 'react';
+
+import {useState} from 'react';
 
 const input = [
   {
-    tabText: 'Tab 1',
-    tabContent: 'Content 1',
-    tabId: 'tab-1'
+    accordionBtn: 'Button 1',
+    accordionContent: 'Content 1',
+    accordionId: 'accordion-1'
   },
   {
-    tabText: 'Tab 2',
-    tabContent: 'Content 2',
-    tabId: 'tab-2'
+    accordionBtn: 'Button 2',
+    accordionContent: 'Content 2',
+    accordionId: 'accordion-2'
   },
   {
-    tabText: 'Tab 3',
-    tabContent: 'Content 3',
-    tabId: 'tab-3'
-  }  
+    accordionBtn: 'Button 3',
+    accordionContent: 'Content 3',
+    accordionId: 'accordion-3'
+  }    
 ];
-
-const firstInputTabId = input[0].tabId;
-const ActiveTabIdContext = createContext(firstInputTabId);
-
-const activeBtn = {
-  backgroundColor: '#000',
-  color: '#fff'
-};
 
 const hide = {
   display: 'none'
@@ -79,61 +52,46 @@ const show = {
   display: 'block'
 };
 
-// Main App
-function Tabs() {
-  const [activeTabId, setActiveTabId] = useState(firstInputTabId);
+function Accordions() {
+  const [openAccordionIds, setOpenAccordionIds] = useState([]);
 
-  useEffect(() => {
-    const hashChangeHandler = () => {
-      const hash = window.location.hash.replace('#', '');
-      const validTab = input.find(tab => tab.tabId === hash);
-      setActiveTabId(validTab ? validTab.tabId : firstInputTabId);
-    };
-
-    hashChangeHandler();
-    window.addEventListener('hashchange', hashChangeHandler);
-
-    return () => {
-      window.removeEventListener('hashchange', hashChangeHandler);
-    };
-  }, []);
-
-  const activeTabIdHandler = (e) => {
-    const id = e.currentTarget.id;
-    window.location.hash = id;
-    setActiveTabId(id);
+  const openAccordionIdsHandler = (e) => {
+    const currentId = e.currentTarget.id;
+    setOpenAccordionIds(prevIds => {
+      if(prevIds.includes(currentId)) {
+        return prevIds.filter(id => id !== currentId);
+      } else {
+        return [...prevIds, currentId];
+      }
+    });
   };
 
   return (
-   <ActiveTabIdContext.Provider value={activeTabId}>
     <div>
-      <div>
-        {input.map(tab => (
-          <TabBtn key={tab.tabId} tab={tab} activeTabIdHandler={activeTabIdHandler} />
-        ))}
-      </div>
-      <div className="tab-content">
-        {input.map(tab => (
-          <TabContent key={tab.tabId} tab={tab} />
-        ))}
-      </div>
-    </div>  
-   </ActiveTabIdContext.Provider>
+      {input.map(accordion => (
+        <div key={accordion.accordionId}>          
+          <AccordionBtn accordion={accordion} openAccordionIdsHandler={openAccordionIdsHandler} openAccordionIds={openAccordionIds} />
+          <AccordionContent accordion={accordion} openAccordionIds={openAccordionIds} />
+        </div>
+      ))}
+    </div>
   );
 }
 
-function TabBtn({tab, activeTabIdHandler}) {
-  const activeTabId = useContext(ActiveTabIdContext);
-
-  return <button role="tab" style={activeTabId === tab.tabId ? activeBtn : {}} aria-selected={activeTabId === tab.tabId ? 'true' : 'false'} aria-controls={tab.tabId} id={tab.tabId} onClick={activeTabIdHandler}>{tab.tabText}</button>;
-}
-
-function TabContent({tab}) {
-  const activeTabId = useContext(ActiveTabIdContext);
-
+function AccordionBtn({accordion, openAccordionIdsHandler, openAccordionIds}) {
   return (
-    <div style={activeTabId === tab.tabId ? show : hide} role="tabpanel" aria-labelledby={tab.tabId}>{tab.tabContent}</div>
+    <div>
+      <button id={accordion.accordionId} aria-expanded={openAccordionIds.includes(accordion.accordionId) ? 'true' : 'false'} onClick={openAccordionIdsHandler}>{accordion.accordionBtn} {openAccordionIds.includes(accordion.accordionId) ? '-' : '+'}</button>
+    </div>
   );
 }
 
-export default Tabs;
+function AccordionContent({accordion, openAccordionIds}) {
+  return (
+    <div aria-labelledby={accordion.accordionId} style={openAccordionIds.includes(accordion.accordionId) ? show : hide}>
+      {accordion.accordionContent}
+    </div>
+  ); 
+}
+
+export default Accordions;
